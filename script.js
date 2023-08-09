dac3abc54fa0c4645a4992d6edfee457184c8d15
Firyal Ghassari <naufal.firyal@gmail.com>
2023-08-09 22:40:37 +0700

    Add notes & minor changes

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const btnApply = document.querySelector(".apply");
 const btnReset = document.querySelector(".reset");
 let squareSize = 8;
 
+// Create 8x8 grid box during page load
 createGrid(squareSize);
 
 function createDiv(size) {
@@ -29,32 +30,6 @@ function createGrid(gridSize) {
   }
 }
 
-btnApply.addEventListener("click", () => {
-  squareSize = Number(gridSize.value);
-
-  while (grid.firstChild) {
-    grid.removeChild(grid.firstChild);
-  }
-
-  createGrid(squareSize);
-})
-
-btnReset.addEventListener("click", () => {
-  const boxes = document.querySelectorAll(".box");
-
-  boxes.forEach(box => {
-    box.style.backgroundColor = "rgb(244, 242, 222)";
-  })
-  
-  gridValue.textContent = `${squareSize}x${squareSize}`;
-  gridSize.value = squareSize;
-})
-
-gridSize.oninput = function() {
-  let value = this.value;
-  gridValue.textContent = `${value}x${value}`;
-}
-
 function colorSelector() {
   let color = [];
   for (let i = 0; i < 3; i++) {
@@ -66,4 +41,33 @@ function colorSelector() {
   } else {
     return "rgb(233, 179, 132)";
   }
+}
+
+// Create grid boxes based on input range
+btnApply.addEventListener("click", () => {
+  squareSize = Number(gridSize.value);
+
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+
+  createGrid(squareSize);
+})
+
+// Reset hover background color to initial
+btnReset.addEventListener("click", () => {
+  const boxes = document.querySelectorAll(".box");
+
+  boxes.forEach(box => {
+    box.style.backgroundColor = "rgb(244, 242, 222)";
+  })
+  
+  gridValue.textContent = `${squareSize}x${squareSize}`;
+  gridSize.value = squareSize;
+})
+
+// Change text on range input
+gridSize.oninput = function() {
+  let value = this.value;
+  gridValue.textContent = `${value}x${value}`;
 }
